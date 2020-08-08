8bbfca00f510bc23fc933021d0e76c3faf5d2e9a
binjin-tw <sakurasakupc@gmail.com>
2020-08-08 10:55:05 +0000

    user registration

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'master_password',
     ];
 
     /**
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'master_password', 'remember_token',
     ];
 
     /**
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,7 +1,7 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
         {{-- トップページへのリンク --}}
-        <a class="navbar-brand" href="/">Accountlist</a>
+        <a class="navbar-brand" href="/">パスワード冷蔵庫</a>
 
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
@@ -10,10 +10,10 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- ユーザ登録ページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">ログインする</a></li>
                 {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">会員登録する</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">ログインする</a></li>
+                {{-- 会員登録ページへのリンク --}}
+                <li>{!! link_to_route('signup.get','会員登録する',[],['class' => 'nav-link']) !!}</li>
             </ul>
         </div>
     </nav>
